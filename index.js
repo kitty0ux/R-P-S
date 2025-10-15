2d58c6ef4b40a995da6327e8c9ef5c138e212589
kitty0ux <xlaetade@gmail.com>
2025-10-15 17:47:00 +0100

    first section of the project.

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+  
+
+function getComputerChoice () {
+ const randomNumber = Math.floor(Math.random() * 3) ;
+
+ if (randomNumber === 0 ) {
+    return "rock";
+ } else if (randomNumber === 1) {
+    return "paper";
+ } else {
+    return "scissors";
+ }
+   }
+   console.log(getComputerChoice());

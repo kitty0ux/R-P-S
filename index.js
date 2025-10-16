c31376861c555408f203760eb0be0626720f9ec4
kitty0ux <xlaetade@gmail.com>
2025-10-16 04:15:25 +0100

    added the funcion to return user choice

--- index.js
+++ index.js
@@ -12,4 +12,19 @@ function getComputerChoice () {
     return "scissors";
  }
    }
-   console.log(getComputerChoice());
+   //console.log(getComputerChoice());
+
+   function getHumanChoice (a, b, c) {
+    let randomChoice = prompt("please enter your choice:");
+
+    if (randomChoice = a ) {
+        return a; 
+    } else if (randomChoice = b ) {
+        return b;
+    } else {
+        return c;
+    }
+   }
+  getHumanChoice("Rock", "Paper", "Scissors");
+
+  console.log(getHumanChoice());
